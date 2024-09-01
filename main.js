87bf5a4c1d45fec314751a8cb663792fd1044cc5
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2024-09-01 10:37:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     res.end('hi');
 });
 
-server.listen(8000, 'localhost', () => {
+server.listen(8000, '0.0.0.0', () => {
     console.log(`Server is running on port 8000`);
 });
 
